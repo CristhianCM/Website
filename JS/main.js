9c4b74b8504776a4b77a8fac774d862f41d84242
Cristhia Carrion <cristhiancarrioncornejo@hotmail.com>
2020-07-05 04:55:34 -0500

    Primer Commit

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+// ---------Menu que aparece cuando  subimos en scroll------------
+
+// let ubicacionPrincipal = window.pageYOffset; //0
+
+// window.addEventListener("scroll", function () {
+//   let desplazamientoActual = window.pageYOffset;
+//   if (ubicacionPrincipal >= desplazamientoActual) {
+//     //200
+//     this.document.getElementsByTagName("nav")[0].style.top = "0px"; // 200 > 100
+//   } else {
+//     document.getElementsByTagName("nav")[0].style.top = "-100px";
+//   }
+//   ubicacionPrincipal = desplazamientoActual; //100
+// });
+AOS.init();
